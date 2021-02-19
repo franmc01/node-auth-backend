ed4a2835ae0e682af732b1c2ccd69b27f3f2105a
francisco-1998 <francmarinc@gmail.com>
2021-02-19 16:57:55 -0500

    Configurando el archivo de variables de entorno y los middlewares de CORS con BodyParser

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 //Importacion de express
 const express = require('express');
 const router = require('./routes/auth.routes');
+const cors = require('cors');
+require('dotenv').config()
 
 //Creación del servidor/aplicación de express
 const app = express();
 
+//Configuracion de CORS
+app.use(cors());
+
+//Configuracion del bodyParser
+app.use(express.json());
 
 //Configuración de las rutas
 app.get('/', (req, res) => {
- return res.json({
-    status: true,
-    msg: "Bienvenido al Auth Backend de Noveno Semestre"
- });
+
+    return res.json({
+        status: true,
+        msg: "Bienvenido al Auth Backend de Noveno Semestre"
+    });
+
 });
+
 app.use('/api/auth', router);
 
-
-
 //Levantar el servidor
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
