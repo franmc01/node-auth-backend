60776688067327761062446480be337128dd1435
francisco-1998 <francmarinc@gmail.com>
2021-02-20 17:02:52 -0500

    Login listo, luego de vomitar por error de capa 8 :'v

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,6 +6,7 @@ const { generarJWT } = require('../helpers/jwt.helper');
 
 //Controlador de la ruta de register
 const crearUsuario = async (req = request, resp = response) => {
+
     //Recepcion de los campos
     const { name, email, password } = req.body;
 
@@ -14,30 +15,30 @@ const crearUsuario = async (req = request, resp = response) => {
         //Verificar el email en la base de datos
         const usuario = await Usuario.findOne({ email });
 
-        if(usuario) {
+        if (usuario) {
             return resp.status(400).json({
-                ok:false,
+                ok: false,
                 msg: 'El email ingresado ya existe en la BD, intente con otro'
             });
         }
 
         //Crear usuario haciendo uso del userSchema
-        const dbusuario = new Usuario( { email, name, password });
+        const dbUsuario = new Usuario({ email, name, password });
 
         //Encriptar la contraseña
         const salt = bcrypt.genSaltSync();
-        dbusuario.password = bcrypt.hashSync(password, salt);
+        dbUsuario.password = bcrypt.hashSync(password, salt);
 
         //Generar el JWT
-        const token = await generarJWT(dbusuario.id, name);
+        const token = await generarJWT(dbUsuario.id, name);
 
         //Guardar el usuario en la base de datos
-        await dbusuario.save();
+        await dbUsuario.save();
 
         //Generar la respuesta exitosa
         return resp.status(201).json({
-            ok:true,
-            uid: dbusuario.id,
+            ok: true,
+            uid: dbUsuario.id,
             name,
             _token: token
         });
@@ -46,26 +47,65 @@ const crearUsuario = async (req = request, resp = response) => {
         return resp.status(500).json({
             ok: true,
             msg: 'Error en servidor, contacte al administrador'
-        })
+        });
     }
 };
 
 //Controlador de la ruta de login
-const loginUsuario = (req = request, resp = response) => {
+const loginUsuario = async (req, resp = response) => {
+    console.log('Llegamos');
 
+    //Recepcion de los campos
     const { email, password } = req.body;
     console.log(email, password);
 
-    return resp.json({
-        status: true,
-        msg: ' Esta ruta logeara a los usuarios'
-    })
+    try {
+
+        //Verificar si existe el usuario en la BD
+        const dbUsuario = await Usuario.findOne({ email });
+
+        //Si no existe, rechazar la petición
+        if (!dbUsuario) {
+            return resp.status(400).json({
+                ok: true,
+                msg: 'El usuario no existe en nuestra BD'
+            });
+        }
+
+        //Si existe, validar las credenciales ingresadas
+        const validPassword = bcrypt.compareSync(password, dbUsuario.password);
+
+        //Si las credenciales son incorrectas, rechazar la petición
+        if (!validPassword) {
+            return resp.status(400).json({
+                ok: true,
+                msg: 'Credenciales de usuario incorrectas'
+            });
+        }
+
+        //Si todo esta ok, generar JWT y dar acceso
+        const token = await generarJWT(dbUsuario.id, dbUsuario.name);
+        return resp.json({
+            ok: true,
+            msg: 'Acceso correcto',
+            uid: dbUsuario.id,
+            name: dbUsuario.name,
+            _token: token
+        })
+
+    } catch (error) {
+        console.log(error);
+        return resp.status(500).json({
+            ok: true,
+            msg: 'Error en servidor, contacte al administrador'
+        })
+    }
 }
 
 //Controlador de la ruta de validar token
 const revalidarToken = (req = request, resp = response) => {
     return response.json({
-        status: true,
+        ok: true,
         msg: ' Esta ruta validará los JWT'
     })
 }
